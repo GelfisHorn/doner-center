49a21daa7cfbe267be193914efc9e02d9eadb4ca
GelfisHorn <helphis.tech@gmail.com>
2023-08-30 16:04:37 -0300

    Design/Introduction section

---
+++ src/app/components/home/Introduction/Introduction.jsx
@@ -0,0 +1,29 @@
+
+// Nextjs
+import Image from 'next/image'
+// Styles
+import styles from './Index.module.css'
+
+export default function Introduction() {
+    return (
+        <div className={`flex items-center justify-center py-20 lg:py-32 ${styles.backgroundImage} lg:min-h-screen select-none px-6 lg:pr-14`}>
+            <div className={`relative lg:left-14 flex flex-col gap-5 w-[40rem] text-center bg-white ${styles.card} p-10`} style={{ zIndex: "2" }}>
+                <div className={"font-detail text-primary text-xl"}>Hallo Schatz</div>
+                <h3 className={"text-3xl"}>Willkommen im Döner Center</h3>
+                <i className={"fa-light fa-pizza text-primary text-xl"}></i>
+                <p className={"text-neutral-600"}>
+                    Willkommen zu einem einzigartigen kulinarischen Erlebnis! Auf unserer Website laden wir Sie ein, eine Auswahl unwiderstehlicher Aromen zu entdecken, die Ihren Gaumen erobern und Ihren Appetit anregen. 
+                    Von wohltuenden Optionen bis hin zu aufregenden Alternativen ist unsere Website Ihr Ziel, um Ihr Verlangen zu stillen.
+                </p>
+            </div>
+            <div className={`flex items-center gap-5`}>
+                <div className={"hidden lg:block relative w-[396px] h-[540px] bg-black"}>
+                    <Image src={"/introduction1.webp"} fill className={"relative h-[540px] w-full object-contain opacity-60"} />
+                </div>
+                <div className={"hidden xl:block relative w-[356px] h-[485px] bg-black"}>
+                    <Image src={"/introduction2.webp"} fill className={"relative h-[485px] w-full object-contain opacity-60"} />
+                </div>
+            </div>
+        </div>
+    )
+}
