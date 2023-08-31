e57717e5d918ca1d061828cb04b97863a810a8a1
GelfisHorn <helphis.tech@gmail.com>
2023-08-31 13:02:36 -0300

    Update/Hero section

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,24 +1,26 @@
 "use client"
 
+// Nextjs
 import Link from "next/link"
 
 export default function Navbar({ absolute, color }) {
     return (
-        <div className={`${absolute ? "absolute top-0 left-0 w-full" : null} ${color ? color : null} flex items-center justify-between h-24 px-6 sm:px-10 lg:px-20`} style={{zIndex: "2"}}>
+        <div className={`${absolute ? "absolute top-0 left-0 w-full" : null} ${color ? color : null} flex items-center justify-between h-24 px-6 sm:px-10 lg:px-20 border-b`} style={{zIndex: "2", borderColor: "rgba(255, 255, 255, .1)"}}>
             <div className={"flex items-center gap-8"}>
-                <h2 className={"text-2xl"}>Döner Center</h2>
+                <Link href={"/"} className={"hover:no-underline"}>
+                    <h2 className={"text-2xl"}>Döner Center</h2>
+                </Link>
             </div>
             <div className={"hidden md:block"}>
-                <ul className={"flex items-center gap-3 uppercase text-lg text-neutral-200"}>
-                    <li><Link href={"/"} className={"hover:no-underline"}>Startseite</Link></li>
-                    <li><Link href={"/#menu"} className={"hover:no-underline"}>Menu</Link></li>
-                    <li><Link href={"/#booktable"} className={"hover:no-underline"}>Reservieren</Link></li>
-                    <li><Link href={"/#kontakt"} className={"hover:no-underline"}>Kontakt</Link></li>
+                <ul className={`flex items-end gap-5 text-lg ${absolute ? "text-neutral-200" : null} font-medium`}>
+                    <li className={""}><Link href={"/"} className={"hover:no-underline"}>Startseite</Link></li>
+                    <li className={""}><Link href={"/#menu"} className={"hover:no-underline"}>Menu</Link></li>
+                    <li className={""}><Link href={"/#kontakt"} className={"hover:no-underline"}>Kontakt</Link></li>
                 </ul>
             </div>
-            {/* <div>
-                <Link href={"/#booktable"} className={"btn-secondary"}>Reservieren</Link>
-            </div> */}
+            <div>
+                <Link href={"/#booktable"} className={"btn-primary"}>Reservieren</Link>
+            </div>
         </div>
     )
 }
--- src/app/components/home/Hero/Hero.jsx
+++ src/app/components/home/Hero/Hero.jsx
@@ -5,16 +5,16 @@ import styles from './Index.module.css'
 
 export default function Hero() {
     return (
-        <div className={`flex items-center ${styles.backgroundImage} text-white text-center lg:text-left min-h-[600px] h-screen`} /* style={{height: 'calc(100vh - 6rem)'}} */>
-            <div className={"relative xl:w-1/2 flex flex-col gap-12 px-6 sm:px-10 lg:px-20 xl:pl-20"} style={{ zIndex: 1 }}>
-                <div className={"flex flex-col gap-5"}>
-                    <div className={"flex flex-col gap-2 uppercase title-1"}>
-                        <div className={"text-5xl sm:text-6xl font-bold"}>Kosten Sie die</div> 
-                        <div className={"text-primary text-4xl"}>Perfektion</div>
+        <div className={`flex items-center text-white text-center min-h-[750px] h-screen`} /* style={{height: 'calc(100vh - 6rem)'}} */>
+            <div className={"relative flex flex-col gap-12 w-full xl:w-2/3 mx-auto px-6 sm:px-10 lg:px-20 xl:pl-20 pt-24"} style={{ zIndex: 1 }}>
+                <div className={"flex flex-col gap-7"}>
+                    <div className={"font-detail text-xl md:text-2xl"}>Unerwiderstehliche Köstlichkeiten</div>
+                    <div className={"flex flex-col gap-2 title-1"}>
+                        <div className={"text-4xl md:text-5xl lg:text-6xl font-[1000]"}><span className={"text-primary"}>Authentische Aromen</span> in jedem Bissen</div>
                     </div>
-                    <p className={"xl:w-2/3 text-neutral-300"}>Entdecken Sie eine Welt voller Geschmack in jedem Bissen. Von außergewöhnlichen Pizzen bis hin zu unvergesslichen Shawarmas entführt Sie GeschmacksFreuden auf eine kulinarische Reise, die Sie nie vergessen werden.</p>
+                    <p className={"text-neutral-300 font-semibold"}>Entdecken Sie eine Welt voller Geschmack in jedem Bissen. Von außergewöhnlichen Pizzen bis hin zu unvergesslichen Shawarmas entführt Sie GeschmacksFreuden auf eine kulinarische Reise, die Sie nie vergessen werden.</p>
                 </div>
-                <div className={"flex flex-col lg:flex-row items-center gap-3 lg:gap-5"}>
+                <div className={"flex flex-col lg:flex-row items-center justify-center gap-3 lg:gap-5"}>
                     <Link href={"/#booktable"} className={"flex items-center btn-primary h-12"}>Einen Tisch reservieren</Link>
                     <Link href={"/#menu"} className={"flex items-center gap-2 btn-secondary h-12"}>
                         Speisekarte
@@ -22,7 +22,6 @@ export default function Hero() {
                     </Link>
                 </div>
             </div>
-            <div className={`w-1/2 ${styles.sideImage}`}></div>
         </div>
     )
 }
