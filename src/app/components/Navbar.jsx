b2e66af0104731b4d3213e513240ebc47dd32de2
Gelfis Horn <130309879+GelfisHorn@users.noreply.github.com>
2023-10-22 17:37:29 +0200

    Update tel

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -26,7 +26,7 @@ export default function Navbar() {
             </div>
             <div>
                 <Link 
-                    href={"tel:+039082933314"} 
+                    href={"tel:+0390 82931673"} 
                     className={"rounded-md px-3 py-2 bg-primary hover:bg-transparent border-2 border-transparent hover:border-primary text-white hover:text-primary hover:no-underline flex items-center gap-2"}
                 >
                     <i className="fa-solid fa-phone"></i>
